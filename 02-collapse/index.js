5db8db1f12f5ff7c02941d3e7fbe8c5813e9c789
Victor <vi-69@yandex.ru>
2022-08-09 19:33:36 +0300

    feat: add animate div

--- 02-collapse/index.js
+++ 02-collapse/index.js
@@ -1,7 +1,21 @@
-const showCollapse = document.getElementsByClassName('collapsible__action--visible')
-const hideCollapse = document.getElementsByClassName('collapsible__action--hidden')
-const anim = document.getElementsByClassName('collapsible__content')
+const showCollapse = document.getElementsByClassName('collapsible__action--visible')[0]
+const hideCollapse = document.getElementsByClassName('collapsible__action--hidden')[0]
+const anim = document.getElementsByClassName('collapsible__content')[0].animate(
+    {
+        height: ["0px", "200px"],
+        width: ['200px','200px'],
+        background: "rgba(255,103,0,0.52)"
+    },
+    {duration: 250, fill: "both", easing: "ease-out"}
+)
+
 console.log(anim)
 console.log(showCollapse)
 console.log(hideCollapse)
 
+hideCollapse.style.display = "none"
+
+
+showCollapse.addEventListener('click', () => {
+    anim.reverse();
+})
