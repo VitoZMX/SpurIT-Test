72f657acdcc6ce7f2cbc45f2ca7b5e30bdd191ee
Victor <vi-69@yandex.ru>
2022-08-09 20:17:23 +0300

    feat: refactor animate div

--- 02-collapse/index.js
+++ 02-collapse/index.js
@@ -1,21 +1,36 @@
 const showCollapse = document.getElementsByClassName('collapsible__action--visible')[0]
 const hideCollapse = document.getElementsByClassName('collapsible__action--hidden')[0]
-const anim = document.getElementsByClassName('collapsible__content')[0].animate(
+const anim = document.getElementsByClassName('collapsible__content')[0].querySelector('p').animate(
     {
         height: ["0px", "200px"],
-        width: ['200px','200px'],
-        background: "rgba(255,103,0,0.52)"
+        width: ['200px', '200px'],
+        opacity: ['0', "1"],
+        background: "rgba(255,103,0,0.52)",
     },
-    {duration: 250, fill: "both", easing: "ease-out"}
+    {duration: 300, fill: "both", easing: "ease-out"}
 )
+
+showCollapse.style.display = "none"
+
+hideCollapse.addEventListener('click', () => {
+    if (anim.playState === "paused") {
+        anim.play();
+    } else {
+        anim.reverse();
+    }
+    showCollapse.style.display = "block"
+    hideCollapse.style.display = "none"
+    console.log("клик показать")
+})
+
+showCollapse.addEventListener('click', () => {
+    anim.reverse();
+    hideCollapse.style.display = "block"
+    showCollapse.style.display = "none"
+    console.log("click скрыть")
+})
 
 console.log(anim)
 console.log(showCollapse)
 console.log(hideCollapse)
-
-hideCollapse.style.display = "none"
-
-
-showCollapse.addEventListener('click', () => {
-    anim.reverse();
-})
+console.log(anim.playState)
